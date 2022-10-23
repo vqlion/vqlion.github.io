d621126a1921ee6554c01fde56475b3e28ed06d2
vql <valentinjossic@outlook.fr>
2022-10-23 23:43:21 +0200

    Add most of the content

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,47 @@
-var title = document.getElementById('title1')
-var appbg = document.getElementById("appbg")
-var social = document.getElementById("social")
-var bg = document.getElementById("bg")
-var clicked = false
+var title = document.getElementById("title1");
+var appbg = document.getElementById("appbg");
+var social = document.getElementById("social");
+var bg = document.getElementById("bg");
+var bigbox = document.getElementById("bigbox");
+var clicked = false;
 //appbg.className = 'clicked'
 //title.style.color = 'black'
 
-title.onmouseover = function() {
-    if(!clicked) {
-        appbg.className = 'hovered'
-        title.style.color = 'black'
-    }
-}
+title.onmouseover = function () {
+  if (!clicked) {
+    appbg.className = "hovered";
+    title.style.color = "black";
+  }
+};
 
-title.onmouseout = function() {
-    if(!clicked) {
-        appbg.className = 'hoverout'
-        title.style.color = 'white'
-    }
-}
+title.onmouseout = function () {
+  if (!clicked) {
+    appbg.className = "hoverout";
+    title.style.color = "white";
+  }
+};
 
-title.onclick = function() {
-    appbg.className = 'clicked'
-    bg.className = 'clicked'
-    social.className = 'show'
-    clicked = true;
-}
+title.onclick = function () {
+  appbg.className = "clicked";
+  bg.className = "clicked";
+  social.className = "show";
+  bigbox.className = "show";
+  clicked = true;
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+        entry.target.classList.add("trans");
+        //social.classList.add("transup");
+        title.classList.add("transup");
+    } else {
+        entry.target.classList.remove("trans");
+        //social.classList.remove("transup");
+        title.classList.remove("transup");
+    }
+
+  });
+});
+
+observer.observe(document.querySelector(".scroll"));
